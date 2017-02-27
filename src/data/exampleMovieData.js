82f5ae2392f8212e7ab372b6f32dea878552e872
Scott <smclendening@gmail.com>
2017-02-26 17:14:11 -0800

    Create repo

---
+++ src/data/exampleMovieData.js
@@ -0,0 +1,11 @@
+window.exampleMovieData = [{
+  title: "Harry Potter",
+}, {
+  title: "The Longest Yard",
+}, {
+  title: "Toy Story",
+}, {
+  title: "Friday the 13th",
+}, {
+  title: "The Hangover",
+}];
